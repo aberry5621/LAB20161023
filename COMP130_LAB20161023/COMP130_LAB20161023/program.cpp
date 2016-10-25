443bf062fbbe0acbab63f5a3a8269ab830c1249f
aberry5621 <aberry5621@mycom.marin.edu>
2016-10-24 17:36:39 -0700

    Welp, probably not gonna need this today.

--- COMP130_LAB20161023/COMP130_LAB20161023/program.cpp
+++ COMP130_LAB20161023/COMP130_LAB20161023/program.cpp
@@ -3,10 +3,20 @@
 // date: octo 24 2016
 // something about binary search
 
-#include <iostream>;
+#include <iostream>
 using namespace std;
 
+// sorts arrays
+void arr_sort(int[], int, int);
+
+// prints arrays
+void arr_print(int[], int, int);
+
 int main() {
+
+	cout << "A binary search exercise" << endl;
+
+	int const ARR_SIZE = 10;
 
 
 
