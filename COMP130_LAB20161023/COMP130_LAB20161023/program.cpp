7e91d74daeb35d3737b717cf7238e180441f95a4
aberry5621 <aberry5621@mycom.marin.edu>
2016-10-24 18:54:57 -0700

    Lab Ex 1, Working Code

--- COMP130_LAB20161023/COMP130_LAB20161023/program.cpp
+++ COMP130_LAB20161023/COMP130_LAB20161023/program.cpp
@@ -3,6 +3,68 @@
 // date: octo 24 2016
 // something about binary search
 
+#define prog 2
+
+#if prog == 2
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+const int COL_SIZE = 4;
+double sum_column(const double matrix[][COL_SIZE], int row_size, int col_idx);
+
+int main() {
+
+	const int ROW_SIZE = 3;
+	const int COL_SIZE = 4;
+	double matrix[ROW_SIZE][COL_SIZE];
+
+	cout << "Enter a 3 - by - 4 matrix, row by row :" << endl;
+
+	for (int row = 0; row < ROW_SIZE; row++) {
+		for (int col = 0; col < COL_SIZE; col++) {
+			cin >> matrix[row][col];
+			cout << "Initializing matrix cell [" << row << "][" << col << "] with " << matrix[row][col] << endl;
+		}
+	}
+
+	for (int i = 0; i < COL_SIZE; i++)
+	{
+		sum_column(matrix, ROW_SIZE, i);
+	}
+
+	return 0;
+
+}
+
+double sum_column(const double matrix[][COL_SIZE], int row_size, int col_idx) {
+
+		double col_total = 0;
+		{
+			for (int row = 0; row < row_size; row++) {
+				col_total += matrix[row][col_idx];
+			}
+			cout << "Sum for column: " << col_idx << " " << col_total << endl;
+		}
+
+		return col_total;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#elif prog == 1
+
 #include <iostream>
 using namespace std;
 
@@ -23,3 +85,4 @@ int main() {
 	return 0;
 
 }
+#endif
